acc2133f6ef25dcde7f25d14f326590cb05228cc
sohan <sohan@appscode.com>
2025-07-30 18:51:19 +0600

    Signed-off-by: sohan <sohan@appscode.com>

--- internal/tracing/trace.go
+++ internal/tracing/trace.go
@@ -5,33 +5,23 @@ import (
 	"log"
 
 	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/exporters/jaeger"
-	"go.opentelemetry.io/otel/sdk/resource"
+	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
-	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
 )
 
-func InitTracer(serviceName string) func(context.Context) error {
-	// Create the Jaeger exporter
-	exp, err := jaeger.New(jaeger.WithCollectorEndpoint(
-		jaeger.WithEndpoint("http://localhost:14268/api/traces"),
-	))
+func InitTracer() func() {
+	// Export trace to stdout
+	exp, err := stdouttrace.New(stdouttrace.WithPrettyPrint())
 	if err != nil {
-		log.Fatalf("failed to create Jaeger exporter: %v", err)
+		log.Fatal(err)
 	}
 
-	// Create a new tracer provider with the exporter
 	tp := sdktrace.NewTracerProvider(
 		sdktrace.WithBatcher(exp),
-		sdktrace.WithResource(resource.NewWithAttributes(
-			semconv.SchemaURL,
-			semconv.ServiceName(serviceName),
-		)),
 	)
-
-	// Register the tracer provider as global
 	otel.SetTracerProvider(tp)
 
-	// Return a shutdown function
-	return tp.Shutdown
+	return func() {
+		_ = tp.Shutdown(context.Background())
+	}
 }
